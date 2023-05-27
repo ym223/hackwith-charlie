7aeb2965a105da2be8f06bdffed91a238985ea46
Yume <poppym0232@gmail.com>
2023-05-27 21:41:58 +0900

    create UI

---
+++ app/src/main/java/jp/chocofac/charlie/ui/page/CreateSenryuScreen.kt
@@ -0,0 +1,149 @@
+package jp.chocofac.charlie.ui.page
+
+import android.Manifest
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+
+@Composable
+fun CreateSenryuScreen() {
+    CreateSenryuContent()
+}
+
+@Composable
+fun CreateSenryuContent() {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        CameraPreview()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "思ったことを書くのじゃ")
+        Spacer(modifier = Modifier.height(16.dp))
+        SenryuTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "川柳を詠むのじゃ")
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPreview() {
+    val multiplePermissionsState = rememberMultiplePermissionsState(buildList {
+        add(Manifest.permission.CAMERA)
+    })
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .height(128.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.surfaceTint
+        )
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            if (multiplePermissionsState.allPermissionsGranted) {
+                // カメラのプレビュー?
+                Text("Preview Image")
+            } else {
+                Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
+                    Text("Request permissions")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SenryuTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = TextStyle.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    cursorBrush: Brush = SolidColor(Color.Black),
+) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(6.dp)
+            ),
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = textStyle.copy(
+            color = MaterialTheme.colorScheme.onSurface,
+        ),
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        visualTransformation = visualTransformation,
+        onTextLayout = onTextLayout,
+        interactionSource = interactionSource,
+        cursorBrush = cursorBrush,
+        singleLine = true,
+        decorationBox = { innerTextField ->
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .padding(horizontal = 36.dp),
+                border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.surfaceTint)
+            ) {
+                innerTextField()
+            }
+        }
+    )
+}
